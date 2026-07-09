dfdf8b0da2caf750d44fba5b4279c8c5bb8ff6fc
kanell0304 <kanell0304@gmail.com>
2026-07-09 16:36:39 +0900

    edit vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Portfolio_2026/',
+  base: process.env.VERCEL ? '/' : '/Portfolio_2026/',
   plugins: [react()],
 });
